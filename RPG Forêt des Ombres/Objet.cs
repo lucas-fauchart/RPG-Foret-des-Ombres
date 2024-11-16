0b078c93eaf34377f66fc65bb37300826f6d5c6d
Lucas Fauchart <lucasfauchart75@gmail.com>
2024-11-16 13:50:31 +0100

    Création de la class objet
    Ajout d'un nom et d'une description ainsi qu'une méthode pour utiliser l'objet.

---
+++ RPG Forêt des Ombres/Objet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Forêt_des_Ombres
+{
+    internal class Objet
+    {
+        //Variable
+        public string nomObjet;
+        public string descriptionObjet;
+
+        //Constructeur
+        public Objet(string leNomObjet, string laDescriptionObjet)
+        {
+            this.nomObjet = leNomObjet;
+            this.descriptionObjet = laDescriptionObjet;
+        }
+
+        //Getters
+        public string GetNomObjet()
+        {
+            return this.nomObjet;
+        }
+        public string GetDescriptionOBJET()
+        {
+            return this.descriptionObjet;
+        }
+
+        // Méthode virtuelle pour permettre une redéfinition
+        public virtual void Utiliser(Heros heros)
+        {
+            Console.WriteLine($"{this.nomObjet} utilisé !");
+        }
+    }
+}
